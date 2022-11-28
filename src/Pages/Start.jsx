01ce08382f7e8c7cba6c442a1309a14089eb8c79
Raul Castro <raul.de.almeida.neto@accenture.com>
2022-11-28 20:46:06 -0300

    criando pages

---
+++ src/Pages/Start.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {View, Text, Image, ScrollView} from "react-native";
+
+export default function Start() {
+    return(
+        <View>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View>
+                    <Image
+                    source={require("../assets/icons/logo3.png")}
+                    ></Image>
+                </View>
+                <Text>
+                    Vamos transformar sua vida {"\n"} em um jogo,
+                    buscando sempre {"\n"} o melhor nível.
+                </Text>
+            </ScrollView>
+        </View>
+    )
+}
