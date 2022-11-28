50e6a3ecc8aa4d1f7f79b532f4ca8be09fdeaad6
Raul Castro <raul.de.almeida.neto@accenture.com>
2022-11-28 20:59:19 -0300

    criando componentes

---
+++ src/Components/Common/LifeStatus/index.jsx
@@ -0,0 +1,19 @@
+import {View, StyleSheet} from "react-native";
+import React from "react"
+
+export default function LifeStatus(){
+      /*Status:
+   100 - Máximo
+   50 - Médio
+   25 - baixo
+   00 - Curto (Acabou o game)
+   No robô, nós temos primeiros Felicidade e depoisa Saúde xx-xx
+*/
+
+return(
+    <View style={StyleSheet.container}>
+
+        
+    </View>
+)
+}
--- src/Pages/Start.jsx
+++ src/Pages/Start.jsx
@@ -3,18 +3,40 @@ import {View, Text, Image, ScrollView} from "react-native";
 
 export default function Start() {
     return(
-        <View>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View>
+        <View style={styles.container}>
+            <ScrollView 
+            style={styles.scroll}
+            showsVerticalScrollIndicator={false}>
+                <View style={{ alignItems: "center" }}>
                     <Image
                     source={require("../assets/icons/logo3.png")}
+                    style={styles.logo}
                     ></Image>
                 </View>
-                <Text>
+                <Text style={styles.description}>
                     Vamos transformar sua vida {"\n"} em um jogo,
                     buscando sempre {"\n"} o melhor nível.
                 </Text>
             </ScrollView>
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "rgba(21, 21, 21, 0.98)",
+    },
+    logo: {
+      width: 300,
+      height: 60,
+      marginTop: 60,
+      marginBottom: 20,
+    },
+    description: {
+      color: "#FFFFFF",
+      fontSize: 20,
+      textAlign: "center",
+      marginVertical: 60,
+    },
+  });
